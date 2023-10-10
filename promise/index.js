6a2225bd574143265beae69bd7c4fd7126eabe28
xiumubai <1547702880@qq.com>
2023-10-10 15:10:47 +0800

    feat: 添加promise链接

---
+++ promise/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2023-10-10 15:09:58
+ * @LastEditors: 1547702880@@qq.com
+ * @LastEditTime: 2023-10-10 15:10:06
+ * @Description: promise原理
+ * @docs: https://juejin.cn/post/6844903625769091079
+ */
+
+
