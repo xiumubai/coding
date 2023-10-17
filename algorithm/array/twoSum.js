75e50ff4bca4b9d9cb774076213c74eac786d9e9
xiumubai <1547702880@qq.com>
2023-10-17 16:24:20 +0800

    feat: 算法目录

---
+++ algorithm/array/twoSum.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2023-10-13 17:05:39
+ * @LastEditors: 1547702880@@qq.com
+ * @LastEditTime: 2023-10-17 15:58:46
+ * @Description: 两数之和
+ */
+
+/**
+ * 题目描述：给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 示例: 给定 nums = [2, 7, 11, 15], target = 9，因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
+ */
+
+function twoSum(array, target) {
+
+
+  return arr
+
+}
---
+++ algorithm/stack/stack.js
@@ -0,0 +1,27 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
