72a4f6019f0f8198472380924b586455f0d22fc9
xiumubai <1547702880@qq.com>
2023-11-21 15:56:06 +0800

    feat: 二叉树

---
+++ algorithm/binary-tree/binary_tree.js
@@ -0,0 +1,29 @@
+const TreeNode = require ('./treeNode')
+
+/* 初始化二叉树 */
+// 初始化节点
+let n1 = new TreeNode(1),
+    n2 = new TreeNode(2),
+    n3 = new TreeNode(3),
+    n4 = new TreeNode(4),
+    n5 = new TreeNode(5);
+// 构建引用指向（即指针）
+n1.left = n2;
+n1.right = n3;
+n2.left = n4;
+n2.right = n5;
+
+console.log(n1);
+
+/* 插入与删除节点 */
+let P = new TreeNode(0);
+// 在 n1 -> n2 中间插入节点 P
+n1.left = P;
+P.left = n2;
+// 删除节点 P
+
+console.log(n1);
+
+n1.left = n2;
+
+console.log(n1);
