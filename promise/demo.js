8764ba6ad8a3374884c0ec27f1b6d3de0bfa91c0
xiumubai <1547702880@qq.com>
2024-06-25 14:53:24 +0800

    feat:  promise面试题

--- promise/demo.js
+++ promise/demo.js
@@ -78,3 +78,58 @@ console.log("script end");
 // promise1 
 // promise2 
 // setTimeout
+
+// 3.
+
+const async1 = async () => {
+  console.log('async1');
+  setTimeout(() => {
+    console.log('timer1')
+  }, 2000)
+  await new Promise(resolve => {
+    console.log('promise1')
+  })
+  console.log('async1 end')
+  return 'async1 success'
+} 
+console.log('script start');
+async1().then(res => console.log(res));
+console.log('script end');
+Promise.resolve(1)
+  .then(2)
+  .then(Promise.resolve(3))
+  .catch(4)
+  .then(res => console.log(res))
+setTimeout(() => {
+  console.log('timer2')
+}, 1000)
+
+// 完整的执行顺序分析：完整的执行顺序为：同步代码的执行
+
+// console.log('script start') 输出：script start
+// 调用 async1() 函数（返回一个 Promise）
+// console.log('async1') 输出：async1
+// 设置一个 2000ms 的定时器，但不会立即执行，加入宏任务队列。
+// console.log('promise1') 输出：promise1
+// await new Promise(resolve => {...})，该 Promise 不会被 resolve，导致 async1 函数暂停在这里。
+// console.log('script end') 输出：script end
+// 处理 Promise 链
+
+// Promise.resolve(1)
+// .then(2) 因为 2 不是一个函数，所以被忽略。
+// .then(Promise.resolve(3)) 同理，Promise.resolve(3) 也被忽略。
+// .catch(4) 也被忽略，因为没有错误。
+// .then(res => console.log(res)) 输出：1，因为 Promise.resolve(1) 的值是 1。
+// 定时器
+
+// 1000ms 后执行 setTimeout(() => console.log('timer2'))，输出：timer2
+// 2000ms 后执行 setTimeout(() => console.log('timer1'))，输出：timer1
+// 结果输出顺序
+// script start
+// async1
+// promise1
+// script end
+// 1
+// timer2 (1秒后)
+// timer1 (2秒后)
+// 注意：由于 await new Promise(resolve => {...}) 永远不会 resolve，因此 console.log('async1 end') 和 console.log('async1 success') 永远不会执行。
