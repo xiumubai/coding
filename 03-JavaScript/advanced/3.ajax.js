c937353c126856d9550b33a4a0d91176b4eda98a
xiumubai <1547702880@qq.com>
2024-06-25 18:48:05 +0800

    feat: 实现AJAX请求

---
+++ 03-JavaScript/advanced/3.ajax.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2023-10-11 17:28:11
+ * @LastEditors: 1547702880@@qq.com
+ * @LastEditTime: 2023-10-11 17:38:32
+ * @Description: 实现AJAX请求
+ */
+const SERVER_URL = "/server";
+let xhr = new XMLHttpRequest();
+// 创建 Http 请求
+xhr.open("GET", SERVER_URL, true);
+// 设置状态监听函数
+xhr.onreadystatechange = function() {
+  if (this.readyState !== 4) return;
+  // 当请求成功时
+  if (this.status === 200) {
+    handle(this.response);
+  } else {
+    console.error(this.statusText);
+  }
+};
+// 设置请求失败时的监听函数
+xhr.onerror = function() {
+  console.error(this.statusText);
+};
+// 设置请求头信息
+xhr.responseType = "json";
+xhr.setRequestHeader("Accept", "application/json");
+// 发送 Http 请求
+xhr.send(null);
