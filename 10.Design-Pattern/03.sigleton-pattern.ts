1f9064faba7d7e6b14a42a0c7e2ec789226063b5
xiumubai <1547702880@qq.com>
2024-07-06 12:52:41 +0800

    feat: 优化单利模式

--- 10.Design-Pattern/03.sigleton-pattern.ts
+++ 10.Design-Pattern/03.sigleton-pattern.ts
@@ -7,18 +7,16 @@
 
 // T extends new (...args: any[] 确保T是一个构造函数
 function singletonPattern<T extends new (...args: any[]) => any>(oldConstructor: T): T {
-  // 唯一实例
   let onlyInstance: InstanceType<T>;
 
-  // 创建一个Proxy来代理构造函数，确保只有一个实例被创建
   const proxyConstructor: T = new Proxy(oldConstructor, {
     construct(target, args) {
-      // 如果存在实例直接返回当前实例
-      if (onlyInstance) return onlyInstance;
-      onlyInstance = Reflect.construct(target, args);
+      if (!onlyInstance) {
+        onlyInstance = Reflect.construct(target, args);
+      }
       return onlyInstance;
     },
-  }) as T;
+  });
 
   return proxyConstructor;
 }
